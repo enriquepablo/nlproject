3a59525c86f22ff64d8d45984b39d5b4a71ff7fb
eperez@gallina.yaco.es <eperez@gallina.yaco.es>
2009-01-09 19:25:06 +0100

    can now split a sentence on several lines

--- nl/bot/bot.py
+++ nl/bot/bot.py
@@ -1,20 +1,21 @@
 """
 This is a generic irc bot, consisting of two modules, this and
 the accompanying resolvers.py. It only depends on twisted; the
-dependency on nl apparent in the fourth line of code is only there
-for application initialization purposes, and can be easily removed.
-And the fifth line could import resolver from wherever.
+dependency on nl apparent in line 24 is only there
+for application initialization purposes (lines 76 & 80),
+and can be easily removed.
+And line 26 could import resolver from wherever.
 The code in this module is almost straight from
 http://www.eflorenzano.com/blog/post/writing-markov-chain-irc-bot-twisted-and-python/,
 and is commented in that link.
-The only added code is, as I said the init code,
-and a call to the resolve function (from resolver) in the
+The only added code is, as I said, the init code,
+and a call to the resolve function (from the resolver module) in the
 privmsg method of MacarronicBot.
 For a synopsis of the use of the resolver module, see its docstring.
 
 This is used, from the command line, like:
 # python bot.py irc_channel
-and it will connect to that channel on freenode.
+and it will connect to #irc_channel on freenode.
 
 """
 
@@ -23,6 +24,9 @@ from twisted.words.protocols import irc
 from twisted.internet import protocol
 import nl
 from nl.bot.resolver import resolve, UnresolvedSentence
+
+partial_msg = ''
+partial = False
 
 class MacarronicBot(irc.IRCClient):
     def _get_nickname(self):
@@ -37,25 +41,31 @@ class MacarronicBot(irc.IRCClient):
         print "Joined %s." % (channel,)
 
     def privmsg(self, user, channel, msg):
-        if not user:
-            return
-        if self.nickname in msg:
+        if user and self.nickname in msg:
             msg = re.compile(self.nickname + "[:,]* ?", re.I).sub('', msg)
             prefix = "%s: " % (user.split('!', 1)[0], )
-        else:
-            prefix = ''
-        try:
-            f, args, kwargs = resolve(msg)
-        except UnresolvedSentence:
-            sentence = 'I do not understand'
-        else:
-            sentence = f(*args, **kwargs)
-        self.msg(self.factory.channel, prefix + sentence)
+            if msg == 'begin':
+                msg = ''
+                partial = True
+            elif msg == 'end':
+                msg = partial_msg
+                partial = False
+            if partial:
+                global partial_msg
+                partial_msg += ' ' + msg
+                return 'yes...'
+            try:
+                f, args, kwargs = resolve(msg)
+            except UnresolvedSentence:
+                sentence = 'I do not understand'
+            else:
+                sentence = f(*args, **kwargs)
+            self.msg(self.factory.channel, prefix + sentence)
 
 class MacarronicBotFactory(protocol.ClientFactory):
     protocol = MacarronicBot
 
-    def __init__(self, channel, nickname='macarronic_bot'):
+    def __init__(self, channel, nickname='luis_ricardo'):
         self.channel = channel
         self.nickname = nickname
 
