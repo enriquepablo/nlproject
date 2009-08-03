cb466df01cf2ee3503fd68564d020aad550247d8
eperez@gallina <eperez@gallina>
2009-08-03 17:58:32 +0200

    corrections

--- nl/examples/cms.py
+++ nl/examples/cms.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with ln.  If not, see <http://www.gnu.org/licenses/>.
 
-from nl import kb, State, Thing, Prop, Rule
+from nl import kb, State, Thing, Prop, Rule, Remove, Equals
 
 # BASIC STUFF
 
@@ -31,18 +31,22 @@ class Wants(State):
     mods = {'to': State}
 
 r1 = Rule('r1', [
-        Prop(Person('X1'), Wants(to=State('X4'))),
+        Prop(Person('X1'), Wants(to=State('X4'))), # XXX only State can be a var
         Prop(Person('X1'), Can(what=State('X4')))
         ],[
         Prop(Person('X1'), State('X4'))])
 
+class Has(State):
+    subject = Person
+    mods = {'what': Thing}
+
 class IsNeeded(State):
-    subject = State
+    subject = Thing
     mods = {'for_action': State}
 
 r2 = Rule('r2', [
-        Prop(State('X2'), IsNeeded(for_action=State('X4'))),
-        Prop(Thing('X1'), State('X2'))
+        Prop(Thing('X2'), IsNeeded(for_action=State('X4'))),
+        Prop(Thing('X1'), Has(what=Thing('X2')))
         ],[
         Prop(Thing('X1'), Can(what=State('X4')))])
 
@@ -55,10 +59,6 @@ r3 = Rule('r3', [
         Prop(Thing('X2'), IsIn(what=Thing('X3')))
         ],[
         Prop(Thing('X1'), IsIn(what=Thing('X3')))])
-
-class Has(State):
-    subject = Person
-    mods = {'what': Thing}
 
 # CONTENT MANAGEMENT
 
@@ -88,7 +88,7 @@ manager = Role('manager')
 r6 = Rule('r6', [
         Person('X1')
         ],[
-        Prop(Person('X1'), Has(member))])
+        Prop(Person('X1'), Has(what=member))])
 
 r9 = Rule('r9', [
         Permission('X2'),
@@ -99,11 +99,6 @@ p1 = Prop(admin, Has(what=manager))
 
 basic_perm = Permission('basic_perm')
 manage_perm = Permission('manage_perm')
-
-r11 = Rule('r11', [
-        Prop(Person('X1'), Has(manager)),
-        ],[
-        Prop(Person('X1'), Has(manage_perm))])
 
 p2 = Prop(member, Has(what=basic_perm))
 
@@ -117,24 +112,25 @@ class IsOwner(State):
     subject = Person
     mods = {'of': Content}
 
-r7 = Rule('r7', [
-        Prop(Person('X1'), Create(what=Content('X2'))),
-        ],[
-        Content('X2'),
-        Prop(Person('X1'), IsOwner(of=Content('X2')))])
-
-create_perm = Permission('create content')
+create_perm = Permission('create_content')
 
 r10 = Rule('r10', [
-        Person('X1'), Wants(what=Create('X2')),
-        Person('X1'), Has(what=create_perm)
+        Prop(Person('X1'), Wants(to=Create(what=Thing('X33')))),
+        Prop(Person('X1'), Has(what=create_perm))
         ],[
-        Person('X1'), Create('X2')])
+        Prop(Person('X1'), Create(what=Thing('X33')))])
 
 class Status(Thing): pass
 
 private = Status('private')
 public = Status('public')
+
+r7 = Rule('r7', [
+        Prop(Person('X1'), Create(what=Content('X2'))),
+        ],[
+        Content('X2'),
+        Prop(Person('X1'), IsOwner(of=Content('X2'))),
+        Prop(Content('X2'), Has(what=private))])
 
 class View(State):
     subject = Person
@@ -143,12 +139,12 @@ class View(State):
 r12 = Rule('r12', [
         Prop(Content('X1'), Has(what=public))
         ],[
-        Prop(Has(what=basic_perm), IsNeeded(for_action=View(what=Content('X1'))))])
+        Prop(basic_perm, IsNeeded(for_action=View(what=Content('X1'))))])
 
 r13 = Rule('r13', [
         Prop(Content('X1'), Has(what=private))
         ],[
-        Prop(Has(what=manage_perm), IsNeeded(for_action(View(what=Content('X1')))))])
+        Prop(manage_perm, IsNeeded(for_action=View(what=Content('X1'))))])
 
 r14 = Rule('r14', [
         Prop(Content('X1'), Has(what=private)),
@@ -161,22 +157,24 @@ class Publish(State):
     mods = {'what': Content,}
 
 r15 = Rule('r15', [
-        Prop(Person('X1'), Publish(what=Content('X2')))
+        Prop(Person('X1'), Publish(what=Content('X2'))),
+        Prop(Content('X2'), Has(what=Status('X4'))),
+        Equals(False, Status('X4'), public)
         ],[
-        Remove(Prop(Content('X2'), Has(what=Status('X3')))),
+        Remove(Prop(Content('X2'), Has(what=Status('X4')))),
         Prop(Content('X2'), Has(what=public))])
 
 r16 = Rule('r16', [
         Content('X1')
         ],[
-        Prop(Has(what=manage_perm), IsNeeded(for_action=Publish(what=Content('X1'))))])
+        Prop(manage_perm, IsNeeded(for_action=Publish(what=Content('X1'))))])
 
 class Hide(State):
     subject = Person
     mods = {'what': Content,}
 
 r17 = Rule('r17', [
-        Prop(Person('X1'), Hide(content=Content('X2')))
+        Prop(Person('X1'), Hide(what=Content('X2')))
         ],[
         Remove(Prop(Content('X2'), Has(what=Status('X3')))),
         Prop(Content('X2'), Has(what=private))])
@@ -188,159 +186,6 @@ r18 = Rule('r18', [
 
 
 kb.tell(admin, member, manager, basic_perm, manage_perm, create_perm, public, private,
-       p1, p2, r1, r2, r3, r4, r5, r6, r7, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18)
+       p1, p2, r10, r1, r2, r3, r4, r5, r6, r7, r9, r12, r13, r14, r15, r16, r17, r18)
 
 
-
-# #####################
-# 
-# 
-# 
-# 
-# 
-# def add_user(name):
-#     """
-#     """
-#     user = Person(name)
-#     kb.tell(user)
-#     kb.extend()
-#     return user
-# 
-# def get_user(name):
-#     """
-#     """
-#     return Person.from_clips(name)
-# 
-# def del_user(name):
-#     """
-#     """
-#     kb.retract(name)
-# 
-# def add_group(name):
-#     """
-#     """
-#     group = Group(name)
-#     kb.tell(group)
-#     kb.extend()
-#     return group
-# 
-# def get_group(name):
-#     """
-#     """
-#     return Group.from_clips(name)
-# 
-# def del_group(name):
-#     """
-#     """
-#     kb.retract(name)
-# 
-# def add_role(name):
-#     """
-#     """
-#     role = Role(name)
-#     kb.tell(role)
-#     kb.extend()
-#     return role
-# 
-# def get_role(name):
-#     """
-#     """
-#     return Role.from_clips(name)
-# 
-# def del_role(name):
-#     """
-#     """
-#     kb.retract(name)
-# 
-# def add_context(name):
-#     """
-#     """
-#     context = Context(name)
-#     kb.tell(context)
-#     kb.extend()
-#     return context
-# 
-# def get_context(name):
-#     """
-#     """
-#     return Context.from_clips(name)
-# 
-# def del_context(name):
-#     """
-#     """
-#     kb.retract(name)
-# 
-# def add_permission(name):
-#     """
-#     """
-#     perm = Permission(name)
-#     kb.tell(perm)
-#     kb.extend()
-#     return perm
-# 
-# def get_permission(name):
-#     """
-#     """
-#     return Permission.from_clips(name)
-# 
-# def del_permission(name):
-#     """
-#     """
-#     kb.retract(name)
-# 
-# def add_to_group(user, group):
-#     """
-#     """
-#     sen = Prop(user, IsIn(group=group), now)
-#     kb.tell(sen)
-#     kb.extend()
-# 
-# def is_in_group(user, group):
-#     """
-#     """
-#     sen = Prop(user, IsIn(group=group), now)
-#     return kb.ask(sen)
-# 
-# def take_from_group(user, group):
-#     """
-#     """
-#     sen = Prop(user, IsIn(group=group), now)
-#     kb.retract(sen)
-# 
-# def give_role(user, role, context):
-#     """
-#     """
-#     sen = Prop(user, Has(role=role, at=context), now)
-#     kb.tell(sen)
-#     kb.extend()
-# 
-# def has_role(user, role, context):
-#     """
-#     """
-#     sen = Prop(user, Has(role=role, at=context), now)
-#     return kb.ask(sen)
-# 
-# def take_role_away(user, role, context):
-#     """
-#     """
-#     sen = Prop(user, Has(role=role, at=context), now)
-#     kb.retract(sen)
-# 
-# def give_permission(user, permission, context):
-#     """
-#     """
-#     sen = Prop(user, Can(perm=permission, at=context), now)
-#     kb.tell(sen)
-#     kb.extend()
-# 
-# def check_permission(user, permission, context):
-#     """
-#     """
-#     sen = Prop(user, Can(perm=permission, at=context), now)
-#     return kb.ask(sen)
-# 
-# def take_permission_away(user, permission, context):
-#     """
-#     """
-#     sen = Prop(user, Can(perm=permission, at=context), now)
-#     kb.retract(sen)
