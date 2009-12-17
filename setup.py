7abbe748e0d17f1251b5ddea35111887a9c8f3a4
Enrique Pérez Arnaud <enriquepablo@gmail.com>
2009-12-17 11:09:09 +0100

    version 0.90

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "nl",
-    version = "0.88",
+    version = "0.90",
     url = 'http://bitbucket.org/enriquepablo/nl/wiki/Home',
     license = 'GPL',
     description = "A python library that provides a production system with an API modelled on the natural language",
@@ -19,6 +19,9 @@ setup(
     zip_safe = False,
     dependency_links = [
         'http://sourceforge.net/project/showfiles.php?group_id=114052',
+        'http://sourceforge.net/projects/pyclips/files/',
+        'http://bitbucket.org/enriquepablo/nl/downloads/',
+        'http://bitbucket.org/enriquepablo/nl/downloads/pyclips-1.0.7.348_clips_6.24-py2.5-linux-i686.egg',
     ],
     install_requires = ['setuptools', 'pyclips'],
     entry_points = {
