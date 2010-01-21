9d0f7952c1f4f431400ed99702e390b3f6870646
Enrique Pérez Arnaud <enriquepablo@gmail.com>
2010-01-21 22:21:24 +0100

    remove pyclips from setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "nl",
-    version = "0.92",
+    version = "0.93",
     url = 'http://bitbucket.org/enriquepablo/nl/wiki/Home',
     license = 'GPL',
     description = "A python library that provides a production system with an API modelled on the natural language",
@@ -18,12 +18,8 @@ setup(
                   ],
     zip_safe = False,
     dependency_links = [
-        'http://sourceforge.net/project/showfiles.php?group_id=114052',
-        'http://sourceforge.net/projects/pyclips/files/',
-        'http://bitbucket.org/enriquepablo/nl/downloads/',
-        'http://bitbucket.org/enriquepablo/nl/downloads/pyclips-1.0.7.348_clips_6.24-py2.5-linux-i686.egg',
     ],
-    install_requires = ['setuptools>=0.6c11', 'pyclips'],
+    install_requires = ['setuptools>=0.6c11',],
     entry_points = {
         'console_scripts':
             [ 'plot_ph22 = nl.examples.physics22:plotPh22', ],
