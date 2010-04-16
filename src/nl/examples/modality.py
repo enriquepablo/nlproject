57bcd6ac868ab2719960bc6242e1870cc53c801a
Enrique Pérez Arnaud <enriquepablo@gmail.com>
2010-04-16 15:22:57 +0200

    adding an issue tracker

---
+++ src/nl/examples/modality.py
@@ -0,0 +1,30 @@
+class Wants(Exists):
+    """
+    the verb Wants has a person proper name as subject
+    and can take as 'to' modifier a verb prhase
+    """
+    subject = Person
+    mods = {'to': Exists}
+
+class Can(Exists):
+    """
+    the verb Can has a person proper name as subject
+    and can take as 'what' modifier a verb prhase
+    """
+    subject = Person
+    instantaneous = False
+    mods = {'what': Exists}
+
+
+class Must(Exists):
+    """
+    the verb Can has a person proper name as subject
+    and can take as 'what' modifier a verb prhase
+    """
+    subject = Person
+    instantaneous = False
+    mods = {'what': Exists}
+
+
+
+
