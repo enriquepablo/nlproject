1bea248ac7b96bab69e68f1eeac81ada7e47a304
Enrique Pérez Arnaud <enriquepablo@gmail.com>
2010-04-26 23:52:33 +0200

    modality

---
+++ src/nl/examples/modality.py
@@ -0,0 +1,33 @@
+from nl import *
+from persons import *
+
+class Wants(Exists):
+    """
+    the verb Wants has a person proper name as subject
+    and can take as 'to' modifier a verb prhase
+    """
+    subject = Person
+    mods = {'to': Exists}
+
+class Can(Exists):
+    """
+    the verb Can has a person proper name as subject
+    and can take as 'what' modifier a verb prhase
+    """
+    subject = Person
+    instantaneous = False
+    mods = {'what': Exists}
+
+
+class Must(Exists):
+    """
+    the verb Can has a person proper name as subject
+    and can take as 'what' modifier a verb prhase
+    """
+    subject = Person
+    instantaneous = False
+    mods = {'what': Exists}
+
+
+
+
