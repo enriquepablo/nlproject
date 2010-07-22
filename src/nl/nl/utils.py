ab941662e7b83c0bb8dc649c8eec87fa85c987b4
enriquepablo <enriquepablo@gmail.com>
2010-07-23 00:58:41 +0200

    let us make time historical

--- src/nl/nl/utils.py
+++ src/nl/nl/utils.py
@@ -75,8 +75,8 @@ def possible_var(var):
         return '?%s' % var
     return var
 
-#_now = float(int(time.time()))
-_now = 1.0
+_now = float(int(time.time()))
+#_now = 1.0
 
 def change_now(i=0):
     'deprecated'
