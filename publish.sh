0e128c257a32fa603adbc5b81d3966db2da688e4
enriquepablo <enriquepablo@gmail.com>
2010-08-05 19:39:54 +0200

    publish docs from the root

--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp -R _static/* static/
-find . -name '*.html' -exec sed -i 's/\<_static\>/static/g' {} \;
-find . -name '*.html' -exec git add {} \;
-git add static/*
-git add searchindex.js
-git commit -m 'updated docs'
-git push origin gh-pages
